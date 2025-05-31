a3391cfba6e6a62320e26a14af316c4bb5ec120d
neopinp <123033639+neopinp@users.noreply.github.com>
2025-05-31 02:21:54 -0400

    (+) Valid Email Format & Username Login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,14 +6,20 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, username } = req.body;
 
   try {
     const hashed = await bcrypt.hash(password, 10);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
     const result = await pool.query(
-      "INSERT INTO users (email, password) values ($1, $2) RETURNING id, email",
-      [email, hashed]
+      "INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING id, email, username",
+      [email, username, hashed]
     );
+
     res.status(201).json({ message: "User registered", user: result.rows[0] });
   } catch (err) {
     if (err.code === "23505") {
@@ -24,10 +30,13 @@ const registerUser = async (req, res) => {
   }
 };
 
-/* ADD CHECKING FOR EMAIL CONDITIONS (NEEDS TO FOLLOW A CERTAIN FORMAT '@' '.com') */
+/* 
+ADD CHECKING FOR EMAIL CONDITIONS (NEEDS TO FOLLOW A CERTAIN FORMAT '@' '.com') 
+USERNAME BASED LOGIN 
+*/
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { emailorUsername, password } = req.body;
 
   // check if the user exists in the database, and if so grant access
   // if user does not exist, produce an error message
@@ -35,8 +44,8 @@ const loginUser = async (req, res) => {
   try {
     // check user
     const result = await pool.query(
-      "SELECT * FROM users WHERE email = $1",
-      [email]
+      "SELECT * FROM users WHERE email = $1 OR username = $1",
+      [emailorUsername]
     );
 
     if (result.rows.length === 0) {
